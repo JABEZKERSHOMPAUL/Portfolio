d4be9da8af97d7392c19dc4c1db51316c081b231
JABEZ <jabpaul2000@gmail.com>
2023-05-10 23:33:49 +0530

    DONE

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,29 +1,29 @@
 import pro1 from "./../image/project1.png"
-import pro2 from "./../image/project2.png"
-import pro3 from "./../image/project3.png"
+import CRUD from "./../image/Screenshot 2023-05-10 215919.png"
+import ToDo from "./../image/Screenshot 2023-05-10 194306.png"
 
 
 
 
 const ProjectCardData = [
+    // {
+    //     imgsrc: pro1,
+    //     title: "Ecommerce Website",
+    //     text: "An ecommerce website is an online platform where businesses can sell their products or services to customers over the internet. These websites typically feature a digital storefront where customers can browse through products, add items to their cart, and complete their purchase using an online payment system.",
+    //     view: "https://www.google.com/",
+    // },
+
     {
-        imgsrc: pro1,
-        title: "Ecommerce Website",
-        text: "An ecommerce website is an online platform where businesses can sell their products or services to customers over the internet. These websites typically feature a digital storefront where customers can browse through products, add items to their cart, and complete their purchase using an online payment system.",
+        imgsrc:CRUD,
+        title: "CRUD Operation",
+        text:"An application that allows users to perform basic database operations on a set of entities or resources. These operations include creating new entities, retrieving existing entities, updating entity data, and deleting entities.",
         view: "https://www.google.com/",
     },
 
     {
-        imgsrc: pro2,
-        title: "Travel Aggregator Website",
-        text:"An international travel package booking website is an online platform that allows travelers to book comprehensive travel packages that include flights, hotels, transportation, and other travel services. These websites are designed to make it easy for travelers to plan and book their international travel arrangements all in one place, saving them time and hassle.",
-        view: "https://www.google.com/",
-    },
-
-    {
-        imgsrc: pro3,
-        title: "WebVideoChat Website",
-        text: "A video call and chat website using socket.io is an online platform that allows users to communicate with each other through real-time video and text chat. Socket.io is a popular JavaScript library that enables real-time, bidirectional communication between web clients and servers, making it an ideal choice for building chat and video call features in a web application.",
+        imgsrc: ToDo,
+        title: "ToDo App",
+        text: " It helps users manage and keep track of their tasks or to-do items. It allows users to create, view, Task Completed, and delete tasks, helping them stay organized and prioritize their work. ",
         view: "https://www.google.com/",
     }
 ];
